d934dd328697e0f1335056a9e5910d9397b4e495
Ivan <ivanulanovskij143@gmail.com>
2021-03-25 16:04:50 +0500

    made my-account block

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,29 @@
-document.querySelector('.header__scroll-to-content').addEventListener('click', () => {
-    window.scrollTo({
-        top: 1005,
-        behavior: "smooth"
-    })
+
+let switcherCircle = document.querySelector('.main__switcher-circle')
+let switcher = document.querySelector('.main__switcher')
+
+switcher.addEventListener('click', () => {
+    //добавляет темную тему
+    if (switcherCircle.classList.contains("main__switcher-circle_dark")) {
+        switcherCircle.classList.remove('main__switcher-circle_dark')
+        switcherCircle.classList.toggle("main__switcher-circle_light")
+        document.querySelector('.main__switcher').style.background = "#0d1117";
+        document.querySelector('.main').style.background = "#0d1117";
+        document.querySelector('.main').style.color = "#c9d1d9"
+        document.querySelector('.link__image').style.background = "#c9d1d9"
+        document.querySelector('.link__image').style.borderRadius = "50%"
+
+    }
+
+    //добавляет светулю тему
+    else {
+        switcherCircle.classList.toggle('main__switcher-circle_dark')
+        switcherCircle.classList.remove("main__switcher-circle_light")
+        document.querySelector('.main__switcher').style.background = "#F5F5F5";
+        document.querySelector('.main').style.background = "#F5F5F5"
+        document.querySelector('.main').style.color = "#0d1117"
+        document.querySelector('.link__image').style.background = "none"
+        document.querySelector('.link__image').style.borderRadius = "0"
+    }
 })
+
